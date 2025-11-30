644cc5b64d64b9f5b449bf1b0c176463225b188e
stefanmzeidler <stefanmzeidler@gmail.com>
2025-11-30 00:08:00 -0600

    Added argument handling.

--- Zeidler_Assignment_1.py
+++ Zeidler_Assignment_1.py
@@ -1,6 +1,25 @@
+import argparse
+
 from patient_retriever import PatientRetriever
+import sys
 
 def main():
-    patient_retriever = PatientRetriever(directory_path="/home/user/PycharmProjects/HI744_Programming_Assignment_1/",nrows=1000)
+    args = check_args()
+    patient_retriever = PatientRetriever(args[0],args[1])
     patient_retriever.find_top_5()
+    patient_retriever.similar_patients_to_json()
+    patient_retriever.metrics_to_json()
+    patient_retriever.plot_precision_recall()
+
+
+def check_args():
+    if (args := sys.argv[1:]) is None:
+        raise TypeError("Please provide arguments")
+    if type(args[0]) != str:
+        raise TypeError("First argument must be a string")
+    if len(args) > 1 and type(args[1]) != int:
+        raise TypeError("Second argument must be a integer")
+    return args
+
+
 main()
