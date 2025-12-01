Fixed precision calculation.

patient_retriever.py
@@ -11,18 +11,19 @@ from sklearn.metrics import precision_recall_fscore_support as prfs
 
 
 class PatientRetriever:
-    def __init__(self, directory_path,nrows=None):
+    def __init__(self, directory_path, nrows=None):
         nltk.download('punkt_tab')
         nltk.download('stopwords')
         self.column_names = ['similar_patients_tfidf', 'similar_patients_doc2vec']
         self.fname = 'PMC-Patients.csv'
-        self.dataset = PatientRetriever._load_dataset(proj_directory = directory_path, fname = self.fname, nrows = nrows)
+        self.dataset = PatientRetriever._load_dataset(proj_directory=directory_path, fname=self.fname, nrows=nrows)
 
     @staticmethod
     def _safe_read_csv(proj_directory: str, fname: str, nrows):
         try:
             filepath = os.path.join(proj_directory, fname)
-            return pd.read_csv(filepath_or_buffer=filepath, usecols = ['patient_uid','patient','similar_patients'],nrows=nrows)
+            return pd.read_csv(filepath_or_buffer=filepath, usecols=['patient_uid', 'patient', 'similar_patients'],
+                               nrows=nrows)
         except Exception as e:
             print(f"An error occurred while reading the file: {e}")
             raise
@@ -51,17 +52,27 @@ class PatientRetriever:
 
     def metrics_to_json(self):
         print("Calculating metrics")
-        self.dataset['similar_patients'] = self.dataset['similar_patients'].apply(lambda x: PatientRetriever._string_to_list(x))
+        self.dataset['similar_patients'] = self.dataset['similar_patients'].apply(
+            lambda x: PatientRetriever._string_to_list(x))
         self.dataset['doc2vec_precision'], self.dataset['doc2vec_recall'] = zip(
-            *self.dataset.apply(lambda row: PatientRetriever._calc_precision_recall(row['similar_patients'], row[self.column_names[1]]),
-                                axis=1))
+            *self.dataset.apply(
+                lambda row: PatientRetriever._calc_precision_recall(row['similar_patients'], row[self.column_names[1]]),
+                axis=1))
         self.dataset['tfidf_precision'], self.dataset['tfidf_recall'] = zip(
-            *self.dataset.apply(lambda row: PatientRetriever._calc_precision_recall(row['similar_patients'], row[self.column_names[0]]),
-                                axis=1))
-        metrics = self.dataset[['patient_uid', 'doc2vec_precision', 'doc2vec_recall', 'tfidf_precision', 'tfidf_recall']]
+            *self.dataset.apply(
+                lambda row: PatientRetriever._calc_precision_recall(row['similar_patients'], row[self.column_names[0]]),
+                axis=1))
+        metrics = self.dataset[
+            ['patient_uid', 'doc2vec_precision', 'doc2vec_recall', 'tfidf_precision', 'tfidf_recall']]
         metrics.to_json('metrics.json', orient='records')
         print("Metrics calculated")
         print(self.dataset.head())
+
+    def calc_overlap(self):
+        print("Calculating overlap")
+        self.dataset['overlap'] = self.dataset.apply(
+            lambda row: len(set(row[self.column_names[0]]) & set(row[self.column_names[1]])), axis=1)
+        return self.dataset['overlap'].mean()
 
     @staticmethod
     def _string_to_list(text):
@@ -76,7 +87,7 @@ class PatientRetriever:
         pred_set = set(y_pred)
         true_positives = len(true_set & pred_set)
         precision = true_positives / len(pred_set) if pred_set else 0
-        recall = true_positives / len(true_set) if true_set else 0
+        recall = true_positives / len(true_set) if true_set else 1
         return precision, recall
 
     def plot_precision_recall(self):
@@ -90,10 +101,9 @@ class PatientRetriever:
         values = np.array([doc2vec_avg_precision, doc2vec_avg_rec, tfidf_avg_precision, tfidf_avg_rec])
         _, ax = plt.subplots()
         bars = ax.bar(range(len(values)), values)
-        ax.bar_label(bars,fmt="%.2f", padding=3)
+        ax.bar_label(bars, fmt="%.2f", padding=3)
         plt.bar(categories, values, color=['tab:blue', 'tab:orange', 'tab:blue', 'tab:orange'])
         plt.xlabel("Document Representation")
         plt.ylabel("Score")
         plt.title("Average Precision and Recall @5 for Doc2Vec and TF-IDF")
         plt.savefig('average_precision_recall.png')
-